20cd059be4b74b96034e2802b45a774d97fda6b9
Sergio-bot-16 <sergioeloycruz@gmail.com>
2025-10-19 18:26:07 -0600

    fix: separar HTML y JS, corregir estructura three.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.154.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.154.0/build/three.module.js';
 
 // --- Configuración inicial ---
 // Crear la escena, cámara y renderizador
